c84c99c3c8e563d993c177197c416af51a7692b3
Yamini S <mithuyam@gmail.com>
2017-03-19 22:42:53 +0530

    First Commit

---
+++ src/main/java/api/Resizable.java
@@ -0,0 +1,27 @@
+package api;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class Resizable {
+public static void main(String[] args) throws InterruptedException
+{
+	
+	System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("http://jqueryui.com/");
+	driver.findElementByLinkText("Sortable").click();
+	/*WebElement x = driver.findElementByXPath("//iframe[@class='demo-frame']");
+	driver.switchTo().frame(x);*/
+    WebElement drag=driver.findElementByXPath("//div[@id='resizable']/div[3]");
+    
+/*   int x= drag.getLocation().getX();
+   int y= drag.getLocation().getY();*/
+   Actions axis=new Actions(driver);
+   axis.clickAndHold(drag).dragAndDropBy(drag, 150, 150).build().perform();
+   Thread.sleep(2000);
+   driver.close();
+}
+}
